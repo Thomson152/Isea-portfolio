fb5d970bf17af45557c7e1e04c50b82b4261e327
Thomson Onyedikachi <tomsinonyedikachi@gmail.com>
2023-09-03 07:28:08 +0100

    new changes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <div className=" py-20  lg:px-10 md:px-3 w-full flex justify-between md:flex-row flex-col ">
+    <div className=" py-20  lg:px-10 md:px-3 px-3 w-full flex justify-between md:flex-row flex-col ">
       <div className=" flex flex-col justify-center px-2 items-center">
         <p className="md:text-5xl text-3xl max-w-3xl text-left leading-tight font">
           Outsmart the competition with best-in-class digital marketing services
@@ -15,13 +15,13 @@ const Contact = () => {
         <div className="bg-[#070e29] md:w-[500px] lg:max-w-[450px] text-white py-10 px-6">
           <form >
             <h1 className="md:text-4xl text-2xl text-center font">Grow Your Brand</h1>
-            <p className="text-lg text-center  mt-1">by partnering with SmartSites</p>
+            <p className="text-lg text-center font mt-1">by partnering with SmartSites</p>
 
             <div className="mt-7 flex flex-col space-y-4">
-              <input type="text" className="py-4 rounded px-2 text-black required outline-none" placeholder="Full Name" />
-              <input type="text" className="py-4 rounded px-2 text-black required outline-none"  placeholder="Email Address" />
-              <input type="text" className="py-4 rounded px-2 text-black aria-required: outline-none"  placeholder="Phone Number" />
-              <button className="ye md:py-4 py-3 md:text-2xl text-xl hover:bg-orange-500 text-black font">Get Your Free Proposal</button>
+              <input type="text" className="py-4 rounded px-2 font text-black required outline-none" placeholder="Full Name" />
+              <input type="text" className="py-4 rounded px-2 font text-black required outline-none"  placeholder="Email Address" />
+              <input type="text" className="py-4 rounded px-2 font text-black aria-required: outline-none"  placeholder="Phone Number" />
+              <button className="ye md:py-4 py-3 md:text-2xl font text-xl hover:bg-orange-500 text-black font">Get Your Free Proposal</button>
             </div>
           </form>
         </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,17 +4,17 @@ const Footer = () => {
       <div className="flex justify-center flex-col px-3 md:px-0 items-center">
         <h2 className="md:text-4xl text-2xl text-white font">Let’s make things happen</h2>
 
-        <p className="md:text-lg mt-2 tezt-[16px] text-center text-slate-200">
+        <p className="md:text-lg font mt-2 tezt-[16px] text-center text-slate-200">
           Partner with the #1 ranked digital marketing agency - before your
           competitor does.
         </p>
         <button className="md:py-5 py-3 ye mt-7 shadow px-[40px] md:px-52 lg:px-72  md:text-2xl text-xl font">
           Get Your Free Proposal
         </button>
-        <p className="mt-5 md:text-2xl text-lg font-sans text-white">
+        <p className="mt-5 md:text-2xl font text-lg font-sans text-white">
           or call{" "}
           <span className="text-yellow-400">
-            <a href="tel:123-456-7890">123-456-7890</a>{" "}
+            <a href="tel:091-680-342-28">091-680-342-28</a>{" "}
           </span>{" "}
           to get started today
         </p>
